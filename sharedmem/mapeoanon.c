8a4c7c1781458b68b406821f1e38b6b1238172c6
carlos <carlos.tafffernaberry@um.edu.ar>
2018-09-04 11:57:59 -0300

    funcionaminto de semaforo

--- sharedmem/mapeoanon.c
+++ sharedmem/mapeoanon.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main (){
 	int pid;
@@ -12,10 +13,14 @@ int main (){
 	if ( pid == 0) { //hijo
 		*ptr = 5;
 		printf("soy el hijo, ya escribi\n");
+		sleep(2);
+		printf ("leido por el hijo %d \n", *ptr);
 		return 0;
 	}
 	//padre
-	sleep(2);
+	sleep(1);
 	printf ("leido %d \n", *ptr);
+	*ptr = (*ptr)*2;
+	wait(NULL);
 	return 0;
 }

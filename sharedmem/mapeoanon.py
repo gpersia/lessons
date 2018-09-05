854616d1dad4ce4d37b3ba8204b4098ab0857bd5
carlostaffe <carlos.taffe@gmail.com>
2018-09-05 17:35:11 -0300

    sincroniza con sleep

--- sharedmem/mapeoanon.py
+++ sharedmem/mapeoanon.py
@@ -7,7 +7,9 @@ ptr = mmap.mmap(-1, mmap.PAGESIZE, mmap.MAP_SHARED, mmap.PROT_WRITE)
 pid = os.fork()
 
 if pid == 0: //hijo
+    time.sleep(1)
     ptr[0]='5'
 else: //padre
-    time.sleep(1)
+    print 'leido de memoria: ' + ptr[0]
+    time.sleep(2)
     print 'leido de memoria: ' + ptr[0]

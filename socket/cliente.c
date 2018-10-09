d848aa4b7ee59a9f35fa6a9af101a8e10ce567fa
carlos <carlos.tafffernaberry@um.edu.ar>
2018-10-09 11:39:30 -0300

    mal ejemplo de tp4

--- socket/cliente.c
+++ socket/cliente.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
 /*struct sockaddr_in {
     short            sin_family;   // e.g. AF_INET
@@ -25,21 +26,28 @@ int main(){
 		return -1;
 	}
 	procrem.sin_family = AF_INET;
-	procrem.sin_port = htons(8080);
+	procrem.sin_port = htons(80);
 	//procrem.sin_addr = 192.168.1.52;
 	//procrem.sin_addr.s_addr = htonl(0xc0a80134);
-	inet_pton(AF_INET,"192.168.1.52", &procrem.sin_addr);
+	//inet_pton(AF_INET,"www.um.eud.ar", &procrem.sin_addr);
+	
+        struct hostent *hp = gethostbyname("www.um.edu.ar");
+	
+	inet_pton(AF_INET,inet_ntoa( *( struct in_addr*)( hp -> h_addr_list[0])), &procrem.sin_addr);
 	conectado = connect(fd,(struct sockaddr *)&procrem, sizeof procrem);
 	if  (conectado < 0 ){
                 perror ("connect");
                 return -1;
         }
-	write(fd, "GET /\n", 6);
-	leido = read(fd, buff , sizeof buff);
-	if  (leido < 0 ){
-		perror ("read");
-		return -1;
-	}
+	write(fd, "GET /es/ua/fi.html HTTP/1.1\nHost: www.um.edu.ar\nConnection: close\n\n", 67);
+	while ((leido = read(fd, buff , sizeof buff)) > 0){
+		if  (leido < 0 ){
+			perror ("read");
+			return -1;
+		}
 	write(1,buff,leido);
+	//parsear palabra
+	}
+
 	return 0;
 }	
--- socket/servidor.c
+++ socket/servidor.c
@@ -6,6 +6,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <netdb.h>
 
 /*struct sockaddr_in {
     short            sin_family;   // e.g. AF_INET
@@ -17,7 +18,7 @@ struct in_addr {
     unsigned long s_addr;  // load with inet_aton()
 };*/
 int main(){
-	int fd, leido, conectado, connfd, pid;
+	int fd, fdcliente, leido, conectado, connfd, pid;
 	char buff[1000];
 	struct sockaddr_in procrem={};
 	fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -46,7 +47,39 @@ int main(){
 		if ( pid == 0 ) {
 			leido = read (connfd,buff ,sizeof buff);
 			write (1 ,buff ,leido);
-			write (connfd ,buff ,leido);
+			//write (connfd ,buff ,leido);
+			// se conecta como cliente a un servidor http
+
+	struct sockaddr_in serverhttp={};
+	fdcliente = socket(AF_INET, SOCK_STREAM, 0);
+	if  (fdcliente < 0 ){
+		perror ("socketcliente");
+		return -1;
+	}
+	serverhttp.sin_family = AF_INET;
+	serverhttp.sin_port = htons(80); //puerto default servidor http
+	//procrem.sin_addr = 192.168.1.52;
+	//procrem.sin_addr.s_addr = htonl(0xc0a80134);
+	//inet_pton(AF_INET,"www.um.eud.ar", &procrem.sin_addr);
+	
+        struct hostent *hp = gethostbyname("www.um.edu.ar");
+	
+	inet_pton(AF_INET,inet_ntoa( *( struct in_addr*)( hp -> h_addr_list[0])), &serverhttp.sin_addr);
+	conectado = connect(fdcliente,(struct sockaddr *)&serverhttp, sizeof serverhttp);
+	if  (conectado < 0 ){
+                perror ("connect");
+                return -1;
+        }		
+	write(fdcliente, "GET /es/ua/fi.html HTTP/1.1\nHost: www.um.edu.ar\nConnection: close\n\n", 67);
+	while ((leido = read(fdcliente, buff , sizeof buff)) > 0){
+		if  (leido < 0 ){
+			perror ("read");
+			return -1;
+		}
+	write(connfd,buff,leido);
+	//parsear palabra
+	}
+
 		return 0;
 		}
 		close(connfd);

0ad89e913b01dfcd2a2b47d5cedf09987197af62
carlost <carlos.taffe@gmail.com>
2019-03-26 01:09:54 -0300

    first commit 2019

--- socket/server.py
+++
@@ -1,8 +0,0 @@
-import socket
-fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM,0)
-fd.bind(("192.168.1.52", 8080))
-fd.listen(5)
-(connfd , direccion) = fd.accept()
-
-connfd.send("url y palabra\n")
-print connfd.recv(100)
